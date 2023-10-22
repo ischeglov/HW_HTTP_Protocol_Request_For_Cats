135e1233adae3b7eeab42ca8ab1c687390d0e069
ischeglov <Ivan_mix@mail.ru>
2023-10-22 21:06:07 +0300

    add basic logic

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Launcher {
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class Launcher {
+
+    public static final String url = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static final ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(url);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        InputStream content = response.getEntity().getContent();
+
+        List<Cat> catList = mapper.readValue(content, new TypeReference<>(){});
+
+        httpClient.close();
+        content.close();
+
+        catList.stream()
+                .filter(cat -> cat.getUpvotes() > 0)
+                .forEach(System.out::println);
+    }
 }
